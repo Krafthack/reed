36efced79d46d09e5110c80a6af1062e57a2e7d7
Ken Grønnbeck <kengroenn@gmail.com>
2015-11-22 15:37:46 +0100

    Use godep save -r for correct save

--- Godeps/_workspace/src/github.com/gorilla/mux/mux_test.go
+++ Godeps/_workspace/src/github.com/gorilla/mux/mux_test.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/gorilla/context"
+	"github.com/krafthack/reed/Godeps/_workspace/src/github.com/gorilla/context"
 )
 
 func (r *Route) GoString() string {
