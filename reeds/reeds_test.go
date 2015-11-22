b59dfa5b5c69e01de5f10176a1be7b7a0abab149
Frode Bjerke <frode.bjerke@gmail.com>
2015-11-22 15:19:35 +0100

    Use a dynamic repository for storing reeds

--- reeds/reeds_test.go
+++ reeds/reeds_test.go
@@ -6,9 +6,15 @@ import (
 
 func TestStoreReed(t *testing.T) {
 
+    repo, err := NewReedsRepo( "localhost", "test", "reedstest" )
+
+    if err != nil {
+        t.Errorf("Could not create reeds repo for tests %d", err)
+    }
+
     reed := Reed{ "Mostly harmless" }
 
-    err := Store(&reed)
+    err = repo.Store(&reed)
 
     if err != nil {
         t.Errorf("No error shuld be thrown got %d", err)
